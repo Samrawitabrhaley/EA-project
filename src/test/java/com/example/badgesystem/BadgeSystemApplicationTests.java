5b15e50172db30674dbdf02e1cefef24c12cfcfa
samrawit <swoldemichael@miu.edu>
2022-05-14 16:53:27 -0500

    new project

---
+++ src/test/java/com/example/badgesystem/BadgeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.badgesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadgeSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
