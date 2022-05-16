7cd3e8f6ed69f2a9a381750efae9676fb07af253
NaGaaDEV <NaGaaDEV@gmail.com>
2022-05-16 12:55:04 -0500

    getall and get by id fo role added

--- src/main/java/ea/badge/controller/RoleController.java
+++ src/main/java/ea/badge/controller/RoleController.java
@@ -5,12 +5,18 @@ import ea.badge.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
+
 @RestController
 @RequestMapping("/roles")
 public class RoleController {
     @Autowired
     private RoleService roleService;
 
+    @GetMapping
+    public Collection<Role> getAll() { return roleService.findAll(); }
+    @GetMapping("/{id}")
+    public Role getById(@PathVariable("id") Integer id) { return roleService.findById(id); }
     @PostMapping
     public void addRole(Role role){
         roleService.addRole(role);
--- src/main/java/ea/badge/service/RoleService.java
+++ src/main/java/ea/badge/service/RoleService.java
@@ -1,8 +1,13 @@
 package ea.badge.service;
 
+import ea.badge.domain.Badge;
 import ea.badge.domain.Role;
 
+import java.util.Collection;
+
 public interface RoleService {
+    Collection<Role> findAll();
+    Role findById(Integer id);
     public void addRole(Role role);
     public void removeRole(Integer id);
 }
