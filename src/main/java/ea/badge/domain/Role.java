367b0945b1df128e8c2bdeeca28e6dbb3520b441
ashishghimire <ashishghimireg@gmail.com>
2022-05-17 14:06:59 -0500

    Added constructor on role

--- src/main/java/ea/badge/domain/Role.java
+++ src/main/java/ea/badge/domain/Role.java
@@ -1,5 +1,6 @@
 package ea.badge.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,8 +12,10 @@ import javax.persistence.Id;
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Role {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+
 }
