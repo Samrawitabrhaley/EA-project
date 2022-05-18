87492c2a55ef4cc3d278d3b8be9389a36e2d71fc
NaGaaDEV <NaGaaDEV@gmail.com>
2022-05-18 18:30:02 -0500

    scan badge with rule

--- src/main/java/ea/badge/service/TransactionServiceImpl.java
+++ src/main/java/ea/badge/service/TransactionServiceImpl.java
@@ -25,8 +25,8 @@ public class TransactionServiceImpl implements TransactionService {
     }
 
     @Override
-    public int getNumberOfTransactionForNumberOfDaysByBadgeId(Long badgeId, int numberOfDays) {
-        return transactionRepository.getNumberOfTransactionForNumberOfDaysByBadgeId(badgeId, LocalDateTime.now().minusDays(numberOfDays));
+    public Collection<Transaction> getTransactionForNumberOfDaysByBadgeId(Long badgeId, int numberOfDays) {
+        return transactionRepository.getTransactionForNumberOfDaysByBadgeId(badgeId, LocalDateTime.now().minusDays(numberOfDays));
     }
 
     @Override
